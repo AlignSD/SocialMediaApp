8677e4a395a18f8426f84f2c71412dd3f61abb3a
Evan Alioto <EvanJAlioto@gmail.com>
2021-04-30 02:43:20 -0700

    started React Front-End by setting up the routes to home, login, and register pages along with basic nav bar

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -30,7 +30,10 @@ module.exports = {
   Mutation: {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
-      console.log(user,'user')
+
+      if (args.body.trim() === '') {
+        throw new Error("Post body must not be empty")
+      }
       const newPost = new Post({
         body,
         user: user.id,

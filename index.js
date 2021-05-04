d5b83a5ac4d18bb19d9aa2adfee2e93b5323a3e3
Evan Alioto <EvanJAlioto@gmail.com>
2021-05-03 22:25:02 -0700

    heroku commit

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const typeDefs= require('./graphql/typeDefs')
 
 const pubsub = new PubSub();
 
+const PORT = process.env.port || 5000;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -15,9 +17,12 @@ const server = new ApolloServer({
 
 mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => {
-    return server.listen({ port:5000 })
+    return server.listen({ port:PORT })
   })
   .then((res) => {
     console.log(`Server Running At ${res.url}`)
+  })
+  .catch(err => {
+    console.error(err)
   })
 
